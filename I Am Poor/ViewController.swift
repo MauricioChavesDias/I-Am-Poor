5c1869722c18b567faca13d23ef0e7fcfeaa3710
MauricioDias <mauriciodyas@gmail.com>
2021-02-17 10:16:17 +1100

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Mauricio Dias on 17/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
